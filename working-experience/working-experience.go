f8bafce32f0c70003fae0c67ef160a9b8046b62a
Gar Mendiola <magahet@gmail.com>
2014-06-27 12:59:51 -0700

    so close

---
+++ working-experience/working-experience.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+)
+
+func processLine(line string) string {
+    for dateRanges := range strings.Split(line, "; ") {
+        dates := strings.Split(dateRanges, "-")
+    }
+}
+
+func readLine(file *os.File) <-chan string {
+	out := make(chan string)
+	go func() {
+		in := bufio.NewReader(file)
+		linePartial := ""
+		for {
+			bytes, isPrefix, err := in.ReadLine()
+			if err != nil {
+				break
+			} else if isPrefix {
+				linePartial += string(bytes)
+			} else {
+				out <- linePartial + string(bytes)
+				linePartial = ""
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("usage:", path.Base(os.Args[0]), "file")
+		os.Exit(1)
+	}
+
+	file, err := os.Open(os.Args[1])
+	defer file.Close()
+
+	if err != nil {
+		fmt.Println("error opening file", os.Args[1], ":", err)
+		os.Exit(1)
+	}
+
+	for line := range readLine(file) {
+		fmt.Println(processLine(line))
+	}
+}
