67aad940333bfe06010f17956e58d8297536ea69
Magahet Mendiola <magahet@gmail.com>
2014-06-29 00:52:22 -0400

    completed easy

--- juggling-with-zeros/juggling-with-zeros.go
+++ juggling-with-zeros/juggling-with-zeros.go
@@ -6,33 +6,30 @@ import (
 	"os"
 	"path"
 	"strings"
-	"time"
+	"strconv"
 )
 
-func iterDates(startStr, endStr string) <-chan time.Time {
-    const dateFormat = "Jan 2006"
-    out := make(chan time.Time)
-    go func() {
-        start, _ := time.Parse(dateFormat, startStr)
-        end, _ := time.Parse(dateFormat, endStr)
-        for now := start; now != end; now = now.AddDate(0, 1, 0) {
-            out <- now
-        }
-        out <- end
-        close(out)
-    }()
-    return out
-}
-
-func processLine(line string) int {
-    monthsWorked := make(map[time.Time]bool)
-    for _, dateRanges := range strings.Split(line, "; ") {
-        dates := strings.Split(dateRanges, "-")
-        for date := range iterDates(dates[0], dates[1]) {
-            monthsWorked[date] = true
+func processLine(line string) int64 {
+    char := ""
+    binStr := ""
+    for i, str := range strings.Fields(line) {
+        // 1 or 0
+        if i %2 == 0 {
+            if len(str) == 1 {
+                char = "0"
+            } else {
+                char = "1"
+            }
+        // how many
+        } else {
+            for j := 0; j < len(str); j++ {
+                binStr += char
+            }
         }
     }
-    return len(monthsWorked) / 12
+    //fmt.Println(binStr)
+    i, _ := strconv.ParseInt(binStr, 2, 64)
+    return i
 }
 
 func readLine(file *os.File) <-chan string {
--- letter-case-percentage-ratio/letter-case-percentage-ratio.go
+++ letter-case-percentage-ratio/letter-case-percentage-ratio.go
@@ -5,34 +5,22 @@ import (
 	"fmt"
 	"os"
 	"path"
-	"strings"
-	"time"
+	"unicode"
 )
 
-func iterDates(startStr, endStr string) <-chan time.Time {
-    const dateFormat = "Jan 2006"
-    out := make(chan time.Time)
-    go func() {
-        start, _ := time.Parse(dateFormat, startStr)
-        end, _ := time.Parse(dateFormat, endStr)
-        for now := start; now != end; now = now.AddDate(0, 1, 0) {
-            out <- now
-        }
-        out <- end
-        close(out)
-    }()
-    return out
-}
-
-func processLine(line string) int {
-    monthsWorked := make(map[time.Time]bool)
-    for _, dateRanges := range strings.Split(line, "; ") {
-        dates := strings.Split(dateRanges, "-")
-        for date := range iterDates(dates[0], dates[1]) {
-            monthsWorked[date] = true
+func processLine(line string) string {
+    totalUpper := 0.0
+    total := 0.0
+    for _, char := range line {
+        if unicode.IsLetter(char) {
+            total++
+            if unicode.IsUpper(char) {
+                totalUpper++
+            }
         }
     }
-    return len(monthsWorked) / 12
+    upperRatio := totalUpper / total
+    return fmt.Sprintf("lowercase: %.2f uppercase: %.2f", 100.0*(1-upperRatio), 100.0*upperRatio)
 }
 
 func readLine(file *os.File) <-chan string {
