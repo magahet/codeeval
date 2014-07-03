677f5f72bfc389e8e7e7f3c0d9e7326ef463ef44
Gar Mendiola <gmendiola@shopzilla.com>
2014-07-02 18:00:06 -0700

    still working on double-squares

--- moderate/double-squares/double-squares.py
+++ moderate/double-squares/double-squares.py
@@ -1,52 +1,81 @@
 #!/usr/bin/env python
 
 import sys
+import random
+from fractions import gcd
 
 
-def gen_primes(maxn):
-    blue_primes = []
-    red_primes = []
-    i = 0
-    p = 0
+def get_prime_factor(N):
+    '''Pollard'''
+    if N % 2 == 0:
+            return 2
+    x = random.randint(1, N - 1)
+    y = x
+    c = random.randint(1, N - 1)
+    g = 1
+    while g == 1:
+            x = ((x * x) % N + c) % N
+            y = ((y * y) % N + c) % N
+            y = ((y * y) % N + c) % N
+            g = gcd(abs(x - y), N)
+    return g
 
-    while p <= int(maxn ** 0.5):
-        p = 4 * i + 3
-        blue_primes.append(p)
-        p = 4 * (i + 1) + 1
-        red_primes.append(p)
-        i += 1
-    return blue_primes, red_primes
+
+def is_prime(n, k=10):
+    '''Miller-Rabin'''
+    if n == 2 or n == 3:
+        return True
+    if not n & 1:
+        return False
+
+    def check(a, s, d, n):
+        x = pow(a, d, n)
+        if x == 1:
+            return True
+        for i in xrange(s - 1):
+            if x == n - 1:
+                return True
+            x = pow(x, 2, n)
+        return x == n - 1
+
+    s = 0
+    d = n - 1
+
+    while d % 2 == 0:
+        d >>= 1
+        s += 1
+
+    for i in xrange(k):
+        a = random.randrange(2, n - 1)
+        if not check(a, s, d, n):
+            return False
+    return True
 
 
 def r2(current):
-    for prime in blue_primes:
-        blue_prime_count = 0
-        while current and current % prime == 0:
-            current /= prime
-            blue_prime_count += 1
-        if blue_prime_count % 2 != 0:
-            return 0
-        if current < prime:
-            break
-        
     a0 = 0
-    while current and current % 2 == 0:
-        current /= 2
-        a0 += 1
-
+    blue_prime_count = 0
     b = 1
-    for prime in red_primes:
-        tr = 0
-        while current and current % prime == 0:
-            current /= prime
-            tr += 1
-        b *= tr + 1
-        if current < prime:
-            break
 
-    if b == 1:
+    while current > 1:
+        prime = get_prime_factor(current)
+        if not is_prime(prime):
+            continue
+        count = 0
+        while current and current % prime == 0:
+            count += 1
+            current /= prime
+
+        if prime == 2:
+            a0 += count
+        elif (prime - 3) % 4 == 0:
+            blue_prime_count += count
+        else:
+            b *= count + 1
+
+    if blue_prime_count % 2 != 0:
         return 0
-    elif b % 2 == 0:
+    if b % 2 == 0:
         return b / 2
     else:
         return (b - (-1 ** a0)) / 2
@@ -55,7 +84,6 @@ def r2(current):
 with open(sys.argv[1]) as _file:
     n_list = [int(l) for l in _file.readlines()]
     n_list.pop(0)
-    
-blue_primes, red_primes = gen_primes(max(n_list))
-for n in n_list:
+
+for i, n in enumerate(n_list):
     print r2(n)
