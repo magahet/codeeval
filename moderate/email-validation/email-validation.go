47d1da984e2044fe0a674d1fe6667cea51b41f58
Magahet Mendiola <magahet@gmail.com>
2014-07-08 13:15:18 -0400

    finish email-validation

--- moderate/email-validation/email-validation.go
+++ moderate/email-validation/email-validation.go
@@ -9,12 +9,66 @@ import (
     "unicode"
 )
 
+var alphaNumericAscii = &unicode.RangeTable{
+	R16: []unicode.Range16{
+	    {'0', '9', 1},
+	    {'A', 'Z', 1},
+	    {'a', 'z', 1},
+	},
+}
+
+var unQuotedSpecials = &unicode.RangeTable{
+	R16: []unicode.Range16{
+	    {33, 33, 1},
+	    {35, 39, 1},
+	    {42, 42, 1},
+	    {43, 43, 1},
+	    {45, 45, 1},
+	    {47, 47, 1},
+	    {61, 61, 1},
+	    {63, 63, 1},
+	    {94, 96, 1},
+	    {123, 126, 1},
+    },
+}
+
+var quotedSpecials = &unicode.RangeTable{
+	R16: []unicode.Range16{
+	    {40, 41, 1},
+	    {44, 44, 1},
+	    {46, 46, 1},
+	    {58, 60, 1},
+	    {62, 62, 1},
+	    {64, 64, 1},
+	    {91, 91, 1},
+	    {93, 93, 1},
+	},
+}
+
+var quotedSpecialsEscaped = &unicode.RangeTable{
+	R16: []unicode.Range16{
+	    {32, 32, 1},
+	    {34, 34, 1},
+	    {92, 92, 1},
+	},
+}
+
 // validate domain component
 func validDomain(domain string) bool {
+    // too long
+    if len(domain) > 253 {
+        return false
+    }
+
     domainParts := strings.Split(domain, ".")
 
     // not enough labels
     if len(domainParts) < 2 {
+        return false
+    }
+
+    // TLD too short
+    if len(domainParts[len(domainParts)-1]) < 2 {
         return false
     }
 
@@ -38,30 +92,89 @@ func validDomain(domain string) bool {
     return true
 }
 
-// validate local component
-func validUser(domain string) bool {
+// validate quoted parts of local
+func validQuotedPart(part string) bool {
+    for i, char := range part {
+        // valid chars for quoted strings
+        if unicode.Is(alphaNumericAscii, char) {
+            continue
+        } else if unicode.Is(unQuotedSpecials, char) {
+            continue
+        } else if unicode.Is(quotedSpecials, char) {
+            continue
+        // valid escaped chars for quoted strings
+        } else if unicode.Is(quotedSpecialsEscaped, char) {
+            // no escape char
+            if i == 0 {
+                return false
+            // escape char present
+            } else if part[i-1] != 0x92 {
+                return false
+            }
+        // not a valid char
+        } else {
+            return false
+        }
+    }
     return true
 }
 
-func processLine(address string) bool {
+// validate unquoted parts of local
+func validUnQuotedPart(part string) bool {
+    for _, char := range part {
+        // not a valid char
+        if unicode.Is(alphaNumericAscii, char) {
+            continue
+        } else if unicode.Is(unQuotedSpecials, char) {
+            continue
+        } else {
+            return false
+        }
+    }
+    return true
+}
+
+// validate local component
+func validUser(user string) bool {
+    // too long
+    if len(user) > 64 {
+        return false
+    }
+
+    if user[0] == '"' && user[len(user)-1] == '"' {
+        return validQuotedPart(user[1:len(user)-1])
+    }
+
+    for _, part := range strings.Split(user, ".") {
+        // consecutive dots or dots at start or end
+        if part == "" {
+            return false
+        }
+        if part[0] == '"' && part[len(part)-1] == '"' {
+            if validQuotedPart(part[1:len(part)-1]) == false {
+                return false
+            }
+        } else if validUnQuotedPart(part) == false {
+            return false
+        }
+    }
+    return true
+}
+
+func validateAddress(address string) bool {
     // too long
     if len(address) > 254 {
         return false
     }
 
     // not made up of local@domain
-    parts := strings.Split(address, "@")
-    if len(parts) != 2 {
+    index := strings.LastIndex(address, "@")
+    if index <= 0 || index == len(address) - 1 {
         return false
     }
 
-    user := parts[0]
-    domain := parts[1]
-
-    // local or domain too long
-    if len(user) > 64 || len(domain) > 253 {
-        return false
-    }
+    user := address[:index]
+    domain := address[index+1:]
 
     if validDomain(domain) == false {
         return false
@@ -113,8 +226,6 @@ func main() {
         if len(line) == 0 {
             continue
         }
-        //fmt.Println(line)
-        fmt.Printf("%v, %s\n", processLine(line), line)
-        //fmt.Println("")
+        fmt.Println(validateAddress(line))
 	}
 }
