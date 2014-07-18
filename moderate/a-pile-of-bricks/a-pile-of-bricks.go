b79d887bcc78b749edab4b536b486abd91e72145
Gar Mendiola <magahet@gmail.com>
2014-07-18 16:30:54 -0700

    wip chain

--- moderate/a-pile-of-bricks/a-pile-of-bricks.go
+++ moderate/a-pile-of-bricks/a-pile-of-bricks.go
@@ -7,74 +7,102 @@ import (
 	"path"
 	"strconv"
 	"strings"
+    "sort"
 )
 
-type Point struct {
-	X, Y int
+type Hole struct {
+	P1, P2 []int
 }
 
-func dist(p1, p2 *Point) int {
-	a := p1.X - p2.X
-	b := p1.Y - p2.Y
-	return a*a + b*b
+func (h *Hole) Delta(i int) int {
+    return Abs(h.P1[i] - h.P2[i])
 }
 
-var r = strings.NewReplacer("[", "", "]", "")
+type Brick struct {
+    Index int
+	P1, P2 []int
+}
+
+func (b *Brick) Delta(i int) int {
+    return Abs(b.P1[i] - b.P2[i])
+}
+
+func Abs(i int) int {
+    if i < 0 {
+        return -i
+    }
+    return i
+}
+
+var r = strings.NewReplacer("[", "", "]", "", "(", "", ")", "")
 
 func parseVector(line string) []int {
     strs := strings.Split(r.Replace(line), ",")
-    vector := make([]int, len(strs)
+    vector := make([]int, len(strs))
     for i, str := range strs {
         vector[i], _ = strconv.Atoi(str)
     }
     return vector
 }
 
-func parseLine(line string) *Hole, []*Bricks {
+func parseLine(line string) (*Hole, []*Brick) {
 	parts := strings.Split(line, "|")
 	v := strings.Fields(parts[0])
-	hole = &Hole{parseVector(v[0]), parseVector(v[1])}
-	v = strings.Fields(parts[0][1:len(parts[0])-1)
-	
-}
-
-func processLine(line string) bool {
-    hole, bricks := parseLine(line)
-	points := make([]*Point, len(parts))
-	var x, y int
-	for i, p := range parts {
-	    xy := strings.Split(r.Replace(p), ",")
-	    x, _ = strconv.Atoi(xy[0])
-	    y, _ = strconv.Atoi(xy[1])
-	    points[i] = &Point{x, y}
-	}
-
-	distMap := make(map[int]int)
-
-	for _, p1 := range points {
-	    for _, p2 := range points {
-	        distMap[dist(p1, p2)]++
-	        if len(distMap) > 3 {
-	            return false
-	        }
-	    }
-	}
-
-    if len(distMap) != 3 {
-        return false
+	hole := &Hole{parseVector(v[0]), parseVector(v[1])}
+	bStr := strings.Split(parts[1], ";")
+    bricks := make([]*Brick, len(bStr))
+    for i, b := range bStr {
+        parts := strings.Fields(r.Replace(b))
+        //fmt.Println(parts)
+        index, _ := strconv.Atoi(parts[0])
+        bricks[i] = &Brick{index, parseVector(parts[1]), parseVector(parts[2])}
     }
 
-    for d, c := range distMap {
-        if d == 0 && c != 4 {
-            return false
-        } else if d != 0 && c != 4 && c != 8 {
+    return hole, bricks
+}
+
+func brickFits(b *Brick, h *Hole) bool {
+    hDeltas := make([]int, 2)
+    bDeltas := make([]int, 3)
+    for i := 0; i < 2; i++ {
+        hDeltas[i] = h.Delta(i)
+    }
+    for i := 0; i < 3; i++ {
+        bDeltas[i] = b.Delta(i)
+    }
+
+    sort.Ints(hDeltas)
+    sort.Ints(bDeltas)
+
+    //fmt.Println(hDeltas, bDeltas)
+
+    for i, hD := range hDeltas {
+        if hD < bDeltas[i] {
             return false
         }
     }
-	//fmt.Println(line)
-	//fmt.Println(distMap)
 
     return true
+}
+
+func processLine(line string) string {
+    hole, bricks := parseLine(line)
+    result := make([]int, 0)
+    for _, b := range bricks {
+        if brickFits(b, hole) {
+            result = append(result, b.Index)
+        }
+    }
+
+    if len(result) == 0 {
+        return "-"
+    }
+    sort.Ints(result)
+    str := ""
+    for _, i := range result {
+        str += fmt.Sprintf("%d,", i)
+    }
+    return strings.Trim(str, ",")
 }
 
 func readLine(file *os.File) <-chan string {
