abefc8f310625f3a3e7cb9f9073663de43b0b6da
Gar Mendiola <gmendiola@shopzilla.com>
2014-07-22 18:36:21 -0700

    completed seq trans

--- moderate/sequence-transformation/sequence-transformation.go
+++ moderate/sequence-transformation/sequence-transformation.go
@@ -5,82 +5,23 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"regexp"
 	"strings"
 )
 
-func isUniform(s string) bool {
-	return numSeq(s) == 1
-}
-
-func numSeq(s string) int {
-	count := 1
-	r := s[0]
-	for i := 1; i < len(s); i++ {
-		if r != s[i] {
-			count++
+func processLine(line string) string {
+	parts := strings.Fields(line)
+	rStr := "^"
+	for _, char := range parts[0] {
+		if char == '0' {
+			rStr += "A+"
+		} else {
+			rStr += `(A+|B+)`
 		}
-		r = s[i]
 	}
-	return count
-}
-
-func numSeqM(s string, m byte) int {
-	if len(s) == 0 {
-		return 0
-	}
-	count := 0
-	r := s[0]
-	if r == m {
-		count++
-	}
-	for i := 1; i < len(s); i++ {
-		if r != s[i] && r == m {
-			count++
-		}
-		r = s[i]
-	}
-	return count
-}
-
-func min(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
-}
-
-func processLine(b, p string, d int) string {
-	if numSeqM(b, byte('1')) < numSeqM(p, byte('B')) {
-		return "No"
-	}
-	if len(b) == 0 && len(p) > 0 {
-		return "No"
-	}
-	if len(b) == 0 && len(p) == 0 {
+	r, _ := regexp.Compile(rStr + "$")
+	if r.MatchString(parts[1]) {
 		return "Yes"
-	}
-	if p[0] == 'B' && b[0] == '0' {
-		return "No"
-	}
-	if p[len(p)-1] == 'B' && b[len(b)-1] == '0' {
-		return "No"
-	}
-	if isUniform(p) && isUniform(b) {
-		return "Yes"
-	}
-
-	i := 1
-	if len(b) > 1 && len(p) > 1 && b[:2] == "10" && p[:2] == "BB" {
-		i = strings.IndexRune(p, 'A')
-	}
-	for ; i < min(len(p), len(b)); i++ {
-		//fmt.Printf("%d,", i)
-		if processLine(b[1:], p[i:], d+1) == "Yes" {
-			return "Yes"
-		}
-		if p[i] != p[0] {
-			break
-		}
 	}
 	return "No"
 }
@@ -122,9 +63,8 @@ func main() {
 
 	for line := range readLine(file) {
 		if line != "" {
-			parts := strings.Fields(line)
-			fmt.Println(line)
-			fmt.Println(processLine(parts[0], parts[1], 1))
+			//fmt.Println(line)
+			fmt.Println(processLine(line))
 		}
 	}
 }
