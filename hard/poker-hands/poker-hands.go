f5d463cae03cb91dc7223a0ae1d10c498ef6606f
Gar Mendiola <magahet@gmail.com>
2014-08-17 20:03:44 -0700

    finished poker

--- hard/poker-hands/poker-hands.go
+++ hard/poker-hands/poker-hands.go
@@ -39,7 +39,23 @@ func (h Hand) GetCardFreq() (map[int]int, []int) {
 	return f, counts
 }
 
-func (h Hand) GetRankValue() int {
+func cardFreqToCountMap(freq map[int]int) map[int][]int {
+    c := make(map[int][]int)
+    for i := 1; i <= 4; i++ {
+        s := make([]int, 0)
+        for card, count := range freq {
+            if count == i {
+                s = append(s, card)
+            }
+        }
+        if len(s) > 0 {
+            c[i] = s
+        }
+    }
+    return c
+}
+
+func GetRankValue(counts []int, h Hand) int {
 	if h.IsStraight && h.IsFlush {
 		if h.Cards[0] == 10 {
 			return 9
@@ -47,14 +63,11 @@ func (h Hand) GetRankValue() int {
 			return 8
 		}
 	}
-	
-	_, counts := h.GetCardFreq()
-	fmt.Println(counts)
-	
+
 	if counts[len(counts)-1] == 4 {
 		return 7
 	}
-	
+
 	if counts[len(counts)-1] == 3 {
 		if counts[0] == 2 {
 			return 6
@@ -62,22 +75,68 @@ func (h Hand) GetRankValue() int {
 			return 3
 		}
 	}
-	
+
 	if h.IsFlush {
 		return 5
 	}
-	
+
 	if h.IsStraight {
 		return 4
 	}
-	
+
 	if counts[len(counts)-1] == 2 {
 		return counts[len(counts)-2]
 	}
-		
+
 	return 0
 }
-	
+
+func maxN(s []int, n int) []int {
+    sort.Ints(s)
+    return s[len(s)-n:]
+}
+
+func GetMajorMinorCards(countMap map[int][]int) (major, minor int) {
+    var mm []int
+    _, ok := countMap[4]
+    if ok {
+        return countMap[4][0], countMap[1][0]
+    }
+    _, ok = countMap[3]
+    if ok {
+        _, ok = countMap[2]
+        if ok {
+            return countMap[3][0], countMap[2][0]
+        } else {
+            return countMap[3][0], maxN(countMap[1], 1)[0]
+        }
+    }
+    _, ok = countMap[2]
+    if ok {
+        if len(countMap[2]) == 2 {
+            mm = maxN(countMap[2], 2)
+            return mm[1], mm[0]
+        } else {
+            return countMap[2][0], maxN(countMap[1], 1)[0]
+        }
+    }
+
+    mm = maxN(countMap[1], 2)
+    return mm[1], mm[0]
+}
+
+func (h Hand) GetValue() uint64 {
+	freq, counts := h.GetCardFreq()
+    rankValue := GetRankValue(counts, h)
+    countMap := cardFreqToCountMap(freq)
+    major, minor := GetMajorMinorCards(countMap)
+    //fmt.Println("parts:", rankValue, major, minor)
+    vStr := fmt.Sprintf("%d%02d%02d%02d%02d%02d%02d%02d", rankValue, major, minor, h.Cards[4], h.Cards[3], h.Cards[2], h.Cards[1], h.Cards[0])
+    //fmt.Println("vstr:", vStr)
+    v, _ := strconv.ParseUint(vStr, 10, 64)
+    return v
+}
+
 func IsConsecutive(seq []int) bool {
 	for i := 0; i < len(seq); i++ {
 		if seq[i] != seq[0] + i {
@@ -109,20 +168,18 @@ func processLine(line string) string {
 	parts := strings.Fields(line)
 	leftHand := newHand(parts[:5])
 	rightHand := newHand(parts[5:])
-	
-	fmt.Println(leftHand, rightHand)
-	fmt.Println(leftHand.GetRankValue(), rightHand.GetRankValue())
-	/*
+
+	//fmt.Println(leftHand, rightHand)
 	leftValue := leftHand.GetValue()
-	rightValue := leftHand.GetValue()
-	
+	rightValue := rightHand.GetValue()
+	//fmt.Println(leftValue, rightValue)
+
 	if leftValue > rightValue {
 		return "left"
 	} else if leftValue < rightValue {
 		return "right"
 	}
-	*/
-	
+
 	return "none"
 }
 
