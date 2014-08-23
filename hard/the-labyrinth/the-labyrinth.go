042248559bfd99381bf1bacc52eefab978a730fd
Gar Mendiola <gmendiola@shopzilla.com>
2014-08-23 01:08:48 -0700

    finished labyrinth

--- hard/the-labyrinth/the-labyrinth.go
+++ hard/the-labyrinth/the-labyrinth.go
@@ -1,116 +1,138 @@
 package main
 
 import (
-    "io/ioutil"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path"
 	"strings"
 )
 
 type Maze struct {
-    M, N int
-    Value string
+	M, N  int
+	Value string
 }
 
 func (a *Maze) String() string {
-    result := ""
-    for i := 0; i < a.M; i++ {
-        result += a.Value[i*a.M:i*a.M+a.N] + "\n"
-    }
-    return result
+	result := ""
+	for i := 0; i < a.M; i++ {
+		result += a.Value[i*a.M:i*a.M+a.N] + "\n"
+	}
+	return strings.Trim(result, "\n")
 }
 
-func (a *Maze) Start() Point {
-    x := strings.Index(a.Value[:a.N], " ")
-    s := Point{x, 0, 0, 0, 0, nil}
-    h := getDist(s, a.End())
-    s.H = h
-    s.F = h
-    return s
+func (a *Maze) getStart() int {
+	return strings.Index(a.Value[:a.N], " ")
 }
 
-func (a *Maze) End() Point {
-    i := strings.Index(a.Value[(a.M-1)*a.N:], " ")
-    return Point{a.M-1, i, 0, 0, 0, nil}
+func (a *Maze) getEnd() int {
+	return strings.Index(a.Value[(a.M-1)*a.N:], " ") + ((a.M - 1) * a.N)
 }
 
-func getNeighborIndexies (i, m, n int) <-chan int {
-    out := make(chan int)
-    go func () {
-        defer close(out)
-        for d := -1; d <= 1; d += 2 {
-            for _, j := range []int{i+d, i+d*n} {
-                if j >= 0 && j < len(m*n) {
-                    out <- j
-                }
-            }
-        }
-    }()
-    return out
-}
-
-func (a *Maze) getAdjecent(p *Point) <- chan Point {
-    out := make(chan Point)
-    go func() {
-        defer close(out)
-        i := p.Y * a.N + p.X
-        var nP Point
-        for j := range getNeighborIndexies(i, a.M, a.N) {
-            if a.Value[i+di] == uint8('*') {
-                continue
-            }
-            nP = Point{(j)%a.N, j/a.N, 0, 0, 0, p}
-            nP.H = getDist(nP, a.End())
-            nP.G = p.G + 1
-            nP.F = nP.H + nP.G
-            out <- nP
-        }
-    }()
-    return out
+func (a *Maze) getAdjecent(i int) <-chan int {
+	out := make(chan int)
+	go func() {
+		defer close(out)
+		for d := -1; d <= 1; d += 2 {
+			for _, j := range []int{i + d, i + d*a.N} {
+				if j >= 0 && j < a.M*a.N && a.Value[j] == uint8(' ') {
+					out <- j
+				}
+			}
+		}
+	}()
+	return out
 }
 
 func Abs(i int) int {
-    if i < 0 {
-        return -i
-    }
-    return i
+	if i < 0 {
+		return -i
+	}
+	return i
 }
 
-func getDist(p1, p2 Point) int {
-    return Abs(p1.X - p2.X) + Abs(p1.Y - p2.Y)
+func (a *Maze) getDist(p1, p2 int) int {
+	x1 := p1 % a.N
+	y1 := p1 / a.N
+	x2 := p2 % a.N
+	y2 := p2 / a.N
+	return Abs(x1-x2) + Abs(y1-y2)
 }
 
 func NewMaze(file string) Maze {
-    rows := strings.Count(file, "\n") + 1
-    cols := strings.Index(file, "\n")
-    return Maze{rows, cols, strings.Replace(file, "\n", "", -1)}
+	rows := strings.Count(file, "\n")
+	cols := strings.Index(file, "\n")
+	return Maze{rows, cols, strings.Replace(file, "\n", "", -1)}
 }
 
-type Point struct {
-    X, Y, F, G, H int
-    Parent *Point
-}
-
-func getMinF(list []Point) Point {
-	index := 0
-	for i, p := range list {
-		if (i > 0) && (p.F <= list[index].F) {
-			index = i
+func getMinF(open map[int]bool, fScores map[int]int) int {
+	minIndex := 0
+	minF := 99999
+	for i := range open {
+		if fScores[i] < minF {
+			minF = fScores[i]
+			minIndex = i
 		}
 	}
-	return list[index]
+	return minIndex
 }
 
-func getPath(a Maze) []Point {
-    open := []Point{a.Start()}
-    var current Point
-    for len(open) > 0 {
-        current = getMinF(open)
-        for _, p := a.getAdjecent(&current) {
-            open = append(open, p)
-        }
-    }
+func (a *Maze) fillPath(cameFrom map[int]int) {
+	path := make(map[int]bool)
+	current := a.getEnd()
+	path[current] = true
+	path[a.getStart()] = true
+	for current, exists := cameFrom[current]; exists; current, exists = cameFrom[current] {
+		path[current] = true
+	}
+	newValue := ""
+	for i, char := range a.Value {
+		if _, isInPath := path[i]; isInPath {
+			newValue += "+"
+		} else {
+			newValue += (string)(char)
+		}
+	}
+	a.Value = newValue
+}
+
+func (a *Maze) findPath() {
+	start := a.getStart()
+	goal := a.getEnd()
+	closed := make(map[int]bool)
+	open := make(map[int]bool)
+	open[start] = true
+	cameFrom := make(map[int]int)
+
+	gScore := make(map[int]int)
+	fScore := make(map[int]int)
+	gScore[start] = 0
+	fScore[start] = a.getDist(start, goal)
+
+	var current int
+	for len(open) > 0 {
+		current = getMinF(open, fScore)
+		if current == goal {
+			a.fillPath(cameFrom)
+			return
+		}
+
+		delete(open, current)
+		closed[current] = true
+
+		for n := range a.getAdjecent(current) {
+			if _, exists := closed[n]; exists {
+				continue
+			}
+
+			if _, exists := open[n]; !exists || gScore[current]+1 < gScore[n] {
+				cameFrom[n] = current
+				gScore[n] = gScore[current] + 1
+				fScore[n] = gScore[n] + a.getDist(n, goal)
+				open[n] = true
+			}
+		}
+	}
 }
 
 func main() {
@@ -118,15 +140,14 @@ func main() {
 		fmt.Println("usage:", path.Base(os.Args[0]), "file")
 		os.Exit(1)
 	}
-	
+
 	file, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
 		fmt.Println("error opening file", os.Args[1], ":", err)
 		os.Exit(1)
 	}
-	
+
 	maze := NewMaze(string(file))
-	//fmt.Println(maze.Start(), maze.End())
-	path := findPath(maze)
-	fmt.Println(path)
+	maze.findPath()
+	fmt.Println(maze.String())
 }
